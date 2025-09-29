5b8c118e0da8153326e546c10b4e55ea7cdea47f
jahnavi2005-abc <djahnavi244@gmail.com>
2025-09-29 21:09:45 +0530

    Add files via upload

---
+++ devops/test_dir/unzipped_dir/sample.py
@@ -0,0 +1,15 @@
+# simple arithmetic in Python
+
+# addition
+a = 10
+b = 5
+print("Sum:", a + b)
+
+# subtraction
+print("Difference:", a - b)
+
+# multiplication
+print("Product:", a * b)
+
+# division
+print("Quotient:", a / b)
